57f75ce129ea4c2b9598312bac265108a8f62691
bk2dcradle <ankitsultana@gmail.com>
2015-12-13 00:39:17 +0530

    Clean

--- spoon.py
+++ spoon.py
@@ -4,8 +4,8 @@ import urllib2
 
 url = 'http://172.18.6.180/ID/Handouts.do'
 
-req = requests.get(url)
-resp = req.text
+req = urllib2.urlopen(url)
+resp = req.read()
 
 soup = BeautifulSoup(resp)
 
@@ -13,20 +13,29 @@ all_anchor = soup.findAll('a')
 
 pattern = re.compile(r'/HANDOUTS/.*\.pdf')
 
-d = 'Party'
+# Set value of variable d like so:
+# d = '/Users/ankitsultana/All-Handouts'
+d = ''
+if d == '':
+    print 'Set value of variable d to the directory required'
+    sys.exit()
 if not os.path.exists(d):
     os.makedirs(d)
 
-os.chdir('/Users/ankitsultana/Desktop/' + d)
+os.chdir(d)
 
 for atags in all_anchor:
     tempurl = 'http://172.18.6.180'
     atagsString = str(atags)
     if re.search('/HANDOUTS/.*\.pdf', atagsString) != None:
+        # Search for /HANDOUTS/CourseCode.pdf
         m = re.search(r'(?<=").*(?=")', atagsString)
+        tempurl += m.group(0)
+
+        # Search for course code
         anotherM = re.search('(?<=HANDOUTS\/).*(?=")', atagsString)
         fileName = anotherM.group(0)
-        tempurl += m.group(0)
+
         another_req = urllib2.urlopen(tempurl)
         data = another_req.read()
         File = open(fileName, 'wb')
